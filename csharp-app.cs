16f120ecfb1fb77ef15d4dcb910beb7f2c40310d
Anufriev Georgy <iamanufrievg@proton.me>
2024-06-23 17:20:49 +0500

    add app on csharp

---
+++ csharp-app.cs
@@ -0,0 +1,14 @@
+namespace DeveloperImproved
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var config = new App()
+			{
+				appId = "..."
+				scope = "http://..."
+			}
+		}
+	}
+}
